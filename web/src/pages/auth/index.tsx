1ae8e7c51d26de822061017c9e32ef56784210b2
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-04-09 17:42:58 +0530

    feat(auth): implement AuthPage with AuthView and Logo components

--- web/src/pages/auth/index.tsx
+++ web/src/pages/auth/index.tsx
@@ -1,3 +1,18 @@
+import { AuthView } from '@daveyplate/better-auth-ui';
+import { useLocation } from 'react-router-dom';
+
+import Logo from '@/components/logo';
+
 export default function AuthPage() {
-  return <div>Page</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <main className="container mx-auto flex h-screen grow flex-col items-center justify-center gap-3">
+      <div className="flex items-center gap-2">
+        <Logo className="text-2xl" />
+      </div>
+
+      <AuthView pathname={pathname} />
+    </main>
+  );
 }
