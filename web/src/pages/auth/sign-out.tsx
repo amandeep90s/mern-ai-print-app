01401fabea98ab87b151a8d64cf0ad1a115bd00e
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-04-09 17:48:36 +0530

    feat(auth): implement SignOutPage with sign-out functionality and loading state

--- web/src/pages/auth/sign-out.tsx
+++ web/src/pages/auth/sign-out.tsx
@@ -1,3 +1,40 @@
+import { useMutation } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Logo from '@/components/logo';
+import { Spinner } from '@/components/ui/spinner';
+import { authClient } from '@/lib/auth-client';
+import { AUTH_ROUTES } from '@/routes/routes';
+
 export default function SignOutPage() {
-  return <div>Page</div>;
+  const navigate = useNavigate();
+
+  const { mutateAsync: signOut, isPending } = useMutation({
+    mutationFn: () => authClient.signOut(),
+    onSuccess: () => {
+      navigate(AUTH_ROUTES.SIGN_IN);
+    },
+  });
+
+  useEffect(() => {
+    signOut();
+  }, [signOut]);
+
+  return (
+    <main className="container mx-auto flex h-screen grow flex-col items-center justify-center gap-3">
+      <div className="flex items-center gap-2">
+        <Logo className="text-2xl" />
+      </div>
+
+      {isPending ? (
+        <div className="flex items-center gap-2">
+          <Spinner />
+          <p>Signing out...</p>
+        </div>
+      ) : (
+        <p>Signed out</p>
+      )}
+    </main>
+  );
 }
